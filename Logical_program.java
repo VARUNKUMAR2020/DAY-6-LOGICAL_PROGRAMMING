eb9c20ab10777ab07578faeba3766dc7c8e4f1c2
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-09-12 07:21:30 +0530

    LOGICAL JAVA PROGRAM

---
+++ Logical_program.java
@@ -0,0 +1,8 @@
+package Logical_programming.day6;
+public class Logical_program 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "WELCOME TO LOGICAL PROGRAMS" );
+    }
+}
